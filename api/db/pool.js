55af1cf284fc961878c166e26c96d4cacabe360a
Allen Mackley <allenmackley@gmail.com>
2018-10-26 16:52:58 -0600

    use nodemon and promise-mysql

--- api/db/pool.js
+++ api/db/pool.js
@@ -1,4 +1,4 @@
-const mysql = require('mysql');
+const mysql = require('promise-mysql');
 //Creating a connection pool allows queries to run in parallel, significantly speeding up operation as an app grows in size.
 module.exports = mysql.createPool({
     //docker-compose makes this local domain name available, which points to the IP for the mysqldb container
